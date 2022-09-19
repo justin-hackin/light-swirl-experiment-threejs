88feddd995bd7ccb3891fb57889d6738bb33df50
Justin Barca <justinbarca@gmail.com>
2022-09-19 19:24:42 -0400

    add controls

--- src/OrbitPointLight.tsx
+++ src/OrbitPointLight.tsx
@@ -10,8 +10,8 @@ function useSawtooth(interval: number) {
   return remainder / interval;
 }
 
-export function OrbitPointLight({ color, zPosition, orbitRadius, interval, offsetRatio = 0 }:
-{ color: string, zPosition: number, orbitRadius: number, interval: number, offsetRatio?: number }
+export function OrbitPointLight({ color, yPosition, orbitRadius, interval, showPosition, offsetRatio = 0 }:
+{ color: string, yPosition: number, orbitRadius: number, interval: number, showPosition: boolean, offsetRatio?: number }
 ) {
   const meshRef = useRef<Mesh>(null!);
   useFrame(() => {
@@ -19,15 +19,21 @@ export function OrbitPointLight({ color, zPosition, orbitRadius, interval, offse
 
     meshRef?.current?.position?.set(
       Math.cos(Math.PI * 2 * positionFraction) * orbitRadius,
-      zPosition,
+      yPosition,
       Math.sin(Math.PI * 2 * positionFraction) * orbitRadius,
     );
   });
 
 
-  return (<mesh ref={meshRef} scale={0.1}>
-    <sphereGeometry  />
-    <meshBasicMaterial color={color} />
-    <pointLight color={color}  />
-  </mesh>);
+  return (
+    <mesh ref={meshRef} scale={0.1}>
+      <pointLight color={color}  />
+      { showPosition && (
+        <mesh>
+          <sphereGeometry  />
+          <meshBasicMaterial color={color} />
+        </mesh>
+      )}
+    </mesh>
+    );
 }
--- src/Scene.tsx
+++ src/Scene.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, useGLTF } from "@react-three/drei";
 // TODO: why '?url' suffix needed when gltf in assetsInclude
+
 import modelUrl from "./models/swirl.gltf?url";
 import { Mesh } from "three";
 import { GLTF } from "three/examples/jsm/loaders/GLTFLoader";
 import { OrbitPointLight } from "./OrbitPointLight";
+import { useTweaks } from "use-tweaks";
 
 type GLTFResult = GLTF & {
   nodes: {
@@ -14,17 +16,24 @@ type GLTFResult = GLTF & {
 }
 
 export function Scene() {
+  const PARAMS = useTweaks({
+    interval1: { value: 13, min: 1, max: 20, },
+    interval2: { value: 20, min: 1, max: 20, },
+    intervalScale: { value: 0.5, min: 0.1, max: 3, },
+    yPosition: { value: 0, min: 0, max: 5, step: 0.1 },
+    showLightPosition: true,
+  });
+
   const { nodes } = useGLTF(modelUrl) as unknown as GLTFResult;
-  const interval = 5;
   const radius = 3;
-  const zPosition = 2;
 
   return (
     <Canvas>
       <OrbitControls />
       <group position={[0, -2, 0]}>
-        <OrbitPointLight color={"#ff00d5"} zPosition={zPosition} interval={interval} orbitRadius={radius} offsetRatio={0.2} />
-        <OrbitPointLight color={"#00e1ff"} zPosition={zPosition} interval={interval} orbitRadius={radius} />
+        <OrbitPointLight
+          color={"#ff00d5"} yPosition={PARAMS.yPosition} interval={PARAMS.interval1 * PARAMS.intervalScale } orbitRadius={radius} offsetRatio={0.5} showPosition={PARAMS.showLightPosition} />
+        <OrbitPointLight color={"#00e1ff"} yPosition={PARAMS.yPosition} interval={PARAMS.interval2 * PARAMS.intervalScale } orbitRadius={radius} showPosition={PARAMS.showLightPosition} />
         <mesh geometry={nodes.Swirl.geometry}>
           <meshStandardMaterial color={"#fff"} />
           <ambientLight intensity={0.1} />
