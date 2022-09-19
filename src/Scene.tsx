a9e7401a77d80762015aece22f87bd388e6b2dfb
Justin Barca <justinbarca@gmail.com>
2022-09-19 14:37:39 -0400

    swirly thing in scene

--- src/Scene.tsx
+++ src/Scene.tsx
@@ -1,19 +1,27 @@
 import React from "react";
 import { Canvas } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls, useGLTF } from '@react-three/drei';
+// TODO: why '?url' suffix needed when gltf in assetsInclude
+import modelUrl from './models/swirl.gltf?url';
+import { Mesh } from "three";
+import { GLTF } from "three/examples/jsm/loaders/GLTFLoader";
+
+type GLTFResult = GLTF & {
+  nodes: {
+    Swirl: Mesh
+  }
+}
 
 export function Scene() {
+  const { nodes } = useGLTF(modelUrl) as unknown as GLTFResult;
   return (
-      <Canvas>
-        <OrbitControls />
-        <mesh>
-          <boxGeometry/>
+    <Canvas>
+      <OrbitControls />
+        <mesh geometry={nodes.Swirl.geometry}>
           <meshStandardMaterial color={'#ff0000'} />
           <ambientLight intensity={0.1} />
           <directionalLight color="red" position={[0, 0, 5]} />
         </mesh>
-      </Canvas>
+    </Canvas>
   )
-
 }
-
