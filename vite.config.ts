0df83e30b88704399a045e07debf456709be2404
Justin Barca <justinbarca@gmail.com>
2022-09-20 13:16:21 -0400

    fix HMR

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.gltf'],
-  server: {
-    hmr: {
-      // port: 443,
-      clientPort: 443
-    }
-  }
 });
